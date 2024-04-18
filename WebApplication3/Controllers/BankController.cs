a7b9d7eea50a7a495389644e61a357a22abcb527
83315 <alshaibaninada@gmail.com>
2024-04-18 10:16:36 +0300

    Add project files.

---
+++ WebApplication3/Controllers/BankController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class BankController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(BranchData.Branches);
+        }
+        public IActionResult Details(int id)
+        {
+            var branches = BranchData.Branches.First(x => x.Id == id);
+
+
+            if (branches == null)
+            {
+                return NotFound();
+            }
+            return View(branches);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(NewBranchForm form)
+        {
+            if (ModelState.IsValid)
+            {
+                var locationName = form.LocationName;
+                var branchManager = form.BranchManager;
+                var employeeCount = form.EmployeeCount;
+                var locationURL = form.LocationURL;
+                BranchData.Branches.Add(new BankBranch { LocationName = locationName, BranchManager = branchManager, EmployeeCount = employeeCount, LocationURL = locationURL });
+                return RedirectToAction("Index", "Bank");
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/NewBranchForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class NewBranchForm
+    {
+        [Required]
+        public string LocationName { get; set; }
+        [Required]
+        public string BranchManager { get; set; }
+        [Required]
+        public int EmployeeCount { get; set; }
+        [Url]
+        public string LocationURL { get; set; }
+
+    }
+}
